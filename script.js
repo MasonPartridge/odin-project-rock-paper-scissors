b2fac463fdd5bdd1a38bd4b7373099314fff2263
MasonPartridge <masonepartridge@gmail.com>
2023-01-15 19:56:46 -0800

    Add basic rock, paper, scissors game.
    Added index.html, script.js, and style.css
    Programmed the logic for the game in script.js leaving index.html and
    style.css empty for the creation of a UI later.

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+1. Take player input
+2. Generate computer input
+3. Turn both inputs into numbers where the higher number beats the lower number apare from a special case where 0 beats 3 with the use of a list.
+4. If both choices are the same tie
+5. If player's choice is higher and computer's choice does not equal 0 or if player's choice equals 0 and the computer's choice is 0 the player wins
+6. else player loses.
+*/
+
+const LISTOFCHOICES = ["ROCK", "PAPER", "SCISSORS"]
+
+function getRoundResults(playerSelectionNum, computerSelectionNum) {
+    if (playerSelectionNum === computerSelectionNum){
+        return "Tie! " + LISTOFCHOICES[playerSelectionNum] + " and " + LISTOFCHOICES[computerSelectionNum] + " are the same"
+    }
+    else if (playerSelectionNum === 0 && computerSelectionNum === 3 || playerSelectionNum > computerSelectionNum && computerSelectionNum != 0){
+        return "You Win! " + LISTOFCHOICES[playerSelectionNum] + " beats " + LISTOFCHOICES[computerSelectionNum];
+    }
+    else {
+        return "You Lose! " + LISTOFCHOICES[computerSelectionNum] + " beats " + LISTOFCHOICES[playerSelectionNum];
+    }
+}
+
+function makeChoiceInputStringIntoNumber(selectionString) {
+    for (var i = 0; i < LISTOFCHOICES.length; i++){
+        if (selectionString === LISTOFCHOICES[i]) {
+            return i;
+        }
+    }
+}
+
+function getComputerChoice () {
+    return Math.floor(Math.random() * 3); 
+}
+
+function playRound () {
+    let playerSelection = prompt("hay dwaddy! Wanna enter Rock, Paper, or Scissors pwease uvu");
+    console.log(getRoundResults(makeChoiceInputStringIntoNumber(playerSelection.toUpperCase()), getComputerChoice()));
+}
+console.log("test");
+
+playRound();
